9c39bc8b531b265d3549e6d5538f94aa5e40ff16
Renato Caiam <caiam.rct@gmail.com>
2015-02-09 12:20:06 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldNotes::Application.config.secret_key_base = '7ce8d50e3123394dff6dc2e90fba4b108815fb48187316fdde7a1dd126318c5abc270022cf6c81ae33cdfd117fd1bc8dd13f39d1cf5e177353412088cccce991'
