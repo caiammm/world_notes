347eba09ae5fc87c5e2b17508c9ec017802b2e84
Renato Caiam <caiam.rct@gmail.com>
2015-05-02 12:44:51 -0300

    muda o tipo de string para text (attr content)

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502150902) do
+ActiveRecord::Schema.define(version: 20150502154150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.string   "tags",       default: [], array: true
     t.integer  "votes"
