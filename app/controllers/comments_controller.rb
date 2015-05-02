8e60c08c09db6746ae2ae12f1e5846e5f8dbc565
Renato Caiam <caiam.rct@gmail.com>
2015-05-02 14:49:29 -0300

    atualiza os controllers e as views dos comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to post_comment_path(@comment.post_id, @comment.id), notice: 'Comment was successfully created.' }
         format.json { render action: 'show', status: :created, location: @comment }
       else
         format.html { render action: 'new' }
@@ -56,7 +56,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url }
+      format.html { redirect_to post_comments_url }
       format.json { head :no_content }
     end
   end
