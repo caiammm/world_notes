4c9cdad96199495776c5f7715d00d6d632befe22
Renato Caiam <caiam.rct@gmail.com>
2015-05-02 11:13:08 -0300

    iniciando repositório limpo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldNotes::Application.config.secret_key_base = 'ca383d145e312386c31d86d12bc1b93d2904f3c83492cecd88ba8bf1e7ed23c3ac02aca28eb2e305db05f41c209a60f183f75fa02e8a92697e9b10640aaff2a5'
