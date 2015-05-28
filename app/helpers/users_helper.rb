80fd678b0ae466dd38a9d5a72e7bef3addd2ef5f
Renato Caiam <caiam.rct@gmail.com>
2015-05-28 20:26:39 -0300

    adiciona helpers para imagem do usuário

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  def gravatar_for_menu(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: 'usuario_menu')
+  end
+
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name)
+  end
+
 end
