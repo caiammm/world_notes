bbdcd27c24fab68ad56d0173de3879ad715147b0
Renato Caiam <caiam.rct@gmail.com>
2015-05-28 20:50:51 -0300

    Atualiza o gravatar com parametros novos

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,15 @@
 module UsersHelper
 
-  def gravatar_for_menu(user)
+  def gravatar_for_menu(user, size=50)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'usuario_menu')
   end
 
 
-  def gravatar_for(user)
+  def gravatar_for(user, size=50)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: user.name)
   end
 
