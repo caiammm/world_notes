904d9f540489fc22e72b47690f8860c153648ace
Renato Caiam <caiam.rct@gmail.com>
2015-05-31 18:58:32 -0300

    alguns ajustes, javascripts e favoritar WIP

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,7 +2,10 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function(){
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+function ready(){
   $('a#button_options').click(function(){
     var target = $(this).attr('class');
     $('div.'+target).toggle();
@@ -13,5 +16,17 @@ $(document).ready(function(){
     $('div.'+target).toggle();
   });
 
+  $('.folder_options').click(function(){
+    var target = $('#new_folder_name');
+    target.attr("disabled","disabled");
+    target.val('')
+  });
 
-});
+  $('.new_folder_options').click(function(){
+    var target = $('#new_folder_name');
+    target.attr("disabled", false);
+    target.val('')
+  });
+
+
+}
