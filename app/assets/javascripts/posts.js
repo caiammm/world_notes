36cf9233f104992aa4ea86d0fcdba6a8e46a9578
Clayton Augusto <cl4yton4ugusto@gmail.com>
2015-05-31 18:58:08 -0300

    Página Show

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,7 +2,7 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function(){
+$(document).on ('page:load', function(){
   $('a#button_options').click(function(){
     var target = $(this).attr('class');
     $('div.'+target).toggle();
